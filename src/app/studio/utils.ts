94f7a125410c2ba8411c564aa52bc1b4390a4229
Yuan Shih <85390677+Yuan0100@users.noreply.github.com>
2025-03-18 15:23:10 +0800

    chores: separate logic of mdx and getPosts to make them more reusable

--- src/app/studio/utils.ts
+++ src/app/studio/utils.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
-import matter from 'gray-matter';
 import { z } from 'zod';
+import { getMDXFiles, parseFrontmatter } from '@/lib/mdx';
 
 const contentDirectory = path.join(process.cwd(), 'src/content/studio');
 
@@ -16,7 +16,7 @@ const MetadataSchema = z.object({
     function: z.string(),
   }).optional(),
   category: z.string(),
-})
+});
 
 export type Metadata = z.infer<typeof MetadataSchema>;
 
@@ -25,41 +25,6 @@ export type Post = {
   metadata: Metadata;
   slug: string;
 };
-
-function parseFrontmatter(fileContent: string, category: string): Post {
-  let { data: metadata, content } = matter(fileContent);
-
-  metadata.category = category;
-
-  const parsedMetadata = MetadataSchema.safeParse(metadata);
-  if (!parsedMetadata.success) {
-    throw new Error(`Invalid front matter: ${parsedMetadata.error.message}`);
-  }
-
-  return {
-    content,
-    metadata: parsedMetadata.data,
-    slug: '', // 會在 getPosts 函數中設置
-  };
-}
-
-function getMDXFiles(dir: string): string[] {
-  let results: string[] = [];
-  const list = fs.readdirSync(dir);
-
-  list.forEach((file) => {
-    const filePath = path.join(dir, file);
-    const stat = fs.statSync(filePath);
-
-    if (stat && stat.isDirectory()) {
-      results = results.concat(getMDXFiles(filePath));
-    } else if (path.extname(file) === '.md' || path.extname(file) === '.mdx') {
-      results.push(filePath);
-    }
-  });
-
-  return results;
-}
 
 export function getStudioPosts(): Post[] {
   const filePaths = getMDXFiles(contentDirectory);
@@ -70,63 +35,15 @@ export function getStudioPosts(): Post[] {
     const fileName = path.basename(filePath);
     const slug = fileName.replace(/\.(md|mdx)$/, '');
 
-    const post = parseFrontmatter(fileContent, category);
-    post.slug = slug;
-
-    return post;
+    const post = parseFrontmatter<Metadata>(fileContent, MetadataSchema, category);
+    return {
+      ...post,
+      slug,
+    };
   });
 }
-
-// ===
 
 export function getPostBySlug(slug: string): Post | undefined {
   const posts = getStudioPosts();
   return posts.find(post => post.slug === slug);
-}
-
-// export function getCategories(): string[] {
-//   const posts = getStudioPosts();
-//   const categories = new Set(posts.map(post => post.metadata.category));
-//   return Array.from(categories);
-// }
-
-// export function getPostsByCategory(category: string): Post[] {
-//   const posts = getStudioPosts();
-//   return posts.filter(post => post.metadata.category === category);
-// }
-
-export function formatDate(date: string, includeRelative = false) {
-  let currentDate = new Date()
-  if (!date.includes('T')) {
-    date = `${date}T00:00:00`
-  }
-  let targetDate = new Date(date)
-
-  let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear()
-  let monthsAgo = currentDate.getMonth() - targetDate.getMonth()
-  let daysAgo = currentDate.getDate() - targetDate.getDate()
-
-  let formattedDate = ''
-
-  if (yearsAgo > 0) {
-    formattedDate = `${yearsAgo}y ago`
-  } else if (monthsAgo > 0) {
-    formattedDate = `${monthsAgo}mo ago`
-  } else if (daysAgo > 0) {
-    formattedDate = `${daysAgo}d ago`
-  } else {
-    formattedDate = 'Today'
-  }
-
-  let fullDate = targetDate.toLocaleString('en-us', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-  })
-
-  if (!includeRelative) {
-    return fullDate
-  }
-
-  return `${fullDate} (${formattedDate})`
 }
--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -1,8 +1,11 @@
+import fs from 'fs';
+import path from 'path';
 import remarkGfm from 'remark-gfm';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 import rehypeShiki from '@shikijs/rehype';
 import { CompileOptions } from '@mdx-js/mdx';
+import matter from 'gray-matter';
 
 export const mdxOptions: CompileOptions = {
   remarkPlugins: [
@@ -14,4 +17,37 @@ export const mdxOptions: CompileOptions = {
     [rehypeShiki, { theme: 'nord' }],
   ],
   format: 'mdx',
+};
+
+export function getMDXFiles(dir: string): string[] {
+  let results: string[] = [];
+  const list = fs.readdirSync(dir);
+
+  list.forEach((file) => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat && stat.isDirectory()) {
+      results = results.concat(getMDXFiles(filePath));
+    } else if (path.extname(file) === '.md' || path.extname(file) === '.mdx') {
+      results.push(filePath);
+    }
+  });
+
+  return results;
+}
+
+export function parseFrontmatter<T>(fileContent: string, schema: any, category: string): { content: string, metadata: T } {
+  const { data: metadata, content } = matter(fileContent);
+  metadata.category = category;
+
+  const parsedMetadata = schema.safeParse(metadata);
+  if (!parsedMetadata.success) {
+    throw new Error(`Invalid front matter: ${parsedMetadata.error.message}`);
+  }
+
+  return {
+    content,
+    metadata: parsedMetadata.data,
+  };
 }
